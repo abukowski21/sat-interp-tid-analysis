151a7a3de9324124cfadee139dd5ed63faac1dc7
Prasoon-codeV <prasoon.vishwakarma10@gmail.com>
2024-09-13 01:35:02 -0500

    Making changes in directories path to accomodate for solstice events

--- Qingyu_Cesar_EIA_IHA/job_read_outgen.py
+++ Qingyu_Cesar_EIA_IHA/job_read_outgen.py
@@ -20,89 +20,94 @@ from p_tqdm import p_map
 from multiprocessing import Pool
 import itertools
 
+month = 'sept'
 
-functions = il.reload(functions)
-month = 'sept' #'march'
+if month == 'sept' or month == 'march':
+    phase = 'equinox'
+else:
+    phase = 'solstice'
 
 if month == 'sept':
-    years_tot = list(range(2010, 2024))
+    years = list(range(2010, 2024))
 else:
-    years_tot = list(range(2010, 2025))
+    years = list(range(2021, 2025))
+
+
+for year in tqdm(years):
+    # Reading the data from output csv
+    grnd_tec2 = pd.read_csv('/home/pxv220016/scratch/Qingyu_Cesar_EIA/outputs/' + month + '/' + str(year) + '_' + month + '_' + phase + '.csv')
+
+
+    # Define the bin edges for MLAT and MLT
+    bins_mlat = pd.cut(grnd_tec2['MLAT'], bins=pd.interval_range(start=-40, end=40, freq=1))
+    bins_mlt = pd.cut(grnd_tec2['MLT'], bins=pd.interval_range(start=0, end=24, freq=0.25))
     
-for year in tqdm(years_tot):
+    # Create a new DataFrame with the bins
+    grnd_tec2['MLAT_b'] = bins_mlat
+    grnd_tec2['MLT_b'] = bins_mlt
     
-    print(year)
+    # Group by the bins (MLAT_b (primary) and MLT_b (secondary)) and calculate the average of TEC
+    result = grnd_tec2.groupby(['MLT_b', 'MLAT_b'])['TEC'].mean().reset_index()
+    # Converting the midpoint values of bins to float and assigning average TEC at those points
+    result['MLAT_b'] = result['MLAT_b'].apply(lambda x: x.mid)
+    result['MLT_b'] = result['MLT_b'].apply(lambda x: x.mid)
+    result['MLAT_b'] = result['MLAT_b'].astype(float)
+    result['MLT_b'] = result['MLT_b'].astype(float)
 
+
+    result = result[result.MLT_b >= 5].reset_index(drop=True)
     
-    # Reading Madrigal Cedar data for +- 21 days around March equinox of 2010-2024
-    # Files in below scratch folder are obtained by using multiple file download 
-    # command in ASCII format and then doing `gunzip file.gz`  
-    path = '/home/pxv220016/scratch/Qingyu_Cesar_EIA/' + month + '_data/' + str(year) + '_' + month + '_equinox/'
-    files = os.listdir(path)
-    files = [path + i for i in files]
-
-    for f in files:
-        if f[-4:] != '.txt':
-            files.remove(f)
-
-    '''
-    for f in files:
-        print(f)
-    '''
-    print(len(files))
     
-    tec_g = []
-    columns = ['GDLAT', 'GLON', 'TEC', 'DT']
-    grnd_tec = pd.DataFrame(columns=columns)
-
-    def process_file(f):
-        # Read function passed continuously during multiple processing to quicken the process
-        df = pd.read_csv(f, sep=r'\s+')
-        d = [dt.datetime(y, m, d, h, mi, s) for y, m, d, h, mi, s in zip(df.YEAR, df.MONTH, df.DAY, df.HOUR, df.MIN, df.SEC)]
-        df['DT'] = d
-        # Dropping unnecassary columns from the Dataframe
-        df = df.drop(['RECNO', 'KINDAT', 'KINST', 'UT1_UNIX', 'UT2_UNIX', 'YEAR', 'MONTH', 'DAY', 'HOUR', 'MIN', 'SEC', 'DTEC'], axis=1)
-        if 'GDALT' in df.columns:
-            df = df.drop(['GDALT'], axis=1)
-        df = df[(df.GDLAT > -60) & (df.GDLAT < 60) & (df.GLON > -85) & (df.GLON < -45)].reset_index(drop=True)
-        return df    
-    # Speeding the process by using parallel processing
-    tec_g = p_map(process_file, files)  # Parallel processing with progress bar
-    print('1')
-    grnd_tec = pd.concat(tec_g, axis=0).reset_index(drop=True)
-    grnd_tec = grnd_tec.sort_values(by=['DT', 'GDLAT'], ascending=[True, True])
-
-
-    # Reading the Kp index values for all the days and filtering undesired points where Kp > 3
-    file = '/home/pxv220016/prasoon/data/sat_interp_repo/repo2/Qingyu_Cesar_EIA_IHA/kp3_index_values/kp_' + str(year) + '_' + month + '.txt'
-    kp = pd.read_csv(file,sep=r'\s+')
-    date_kp = [functions.day_to_date(i, year) for i in kp.DOY]
-    m, d = zip(*date_kp)
-    kp['date'] = [dt.datetime(year, j, i, k, 0, 0) for i,j,k in zip(d,m,kp.Hour)]
-    kp['kp'] = [i/10 for i in kp.Kp]
-    kp = kp.drop(['Year', 'DOY', 'Hour','Kp'], axis = 1)
-    grnd_tec0 = functions.kp_index_filtering(grnd_tec, kp)
-    print('2')
-
-    # Calculation of magnetic coordinates by using Apex library and Parallel prcoessing 
-    t_start = dt.datetime.now() # just a timer
-    with Pool(20) as pool:
-        p = pool.starmap(functions.magnetic_coords_parallel, zip(grnd_tec0.DT, grnd_tec0.GDLAT, grnd_tec0.GLON, grnd_tec0.TEC))
-    pool.close()
-    pool.join()
-    # Separating the data from output list 
-    sat_date, sat_glat, sat_glon, sat_tec, sat_mlat, sat_mlon, sat_mlt = zip(*p)
-    t_total = dt.datetime.now() - t_start
-    print(t_total)
+    filtered = []
+    for t in result['MLT_b'].unique():
+        result_f = result[result['MLT_b'] == t].reset_index(drop=True)
+        fit = savgol_filter(np.array(result_f.TEC), 10, 2)
+        filtered.extend(fit)
+    result['TEC'] = filtered
+    result = result.groupby(['MLAT_b', 'MLT_b']).sum().reset_index()
     
+    
+    # Identifying NH and SH peaks
+    result_t = result[result.MLT_b >= 13].reset_index(drop=True)
+    result_n = result_t[result_t.MLAT_b > 0].reset_index(drop=True)
+    result_s = result_t[result_t.MLAT_b < 0].reset_index(drop=True)
+    result_n = result_n.loc[result_n.groupby('MLT_b')['TEC'].idxmax()].reset_index(drop=True)
+    result_s = result_s.loc[result_s.groupby('MLT_b')['TEC'].idxmax()].reset_index(drop=True)
+    # Dropping the cases where the SH peak is not prominent and maxima appears to come at equator
+    result_n.loc[result_n['MLAT_b'] < 8, 'TEC'] = np.nan
+    result_s.loc[result_s['MLAT_b'] > -8, 'TEC'] = np.nan
+    #print(result_s)
+    
+    i = 100*2*(result_n.TEC - result_s.TEC)/(result_n.TEC + result_s.TEC)
+    result_ind = pd.DataFrame({'mlat_n': result_n.MLAT_b, 'tec_n': result_n.TEC, 'mlat_s': result_s.MLAT_b, 'tec_s': result_s.TEC, 'mlt': result_n.MLT_b, 'asy': i})
+    result_ind.to_csv('/home/pxv220016/prasoon/data/sat_interp_repo/repo2/Qingyu_Cesar_EIA_IHA/outputs/' + month + '/asy_' + str(year) + '_' + month + '.csv', index=False)
 
-    # Reordering the outputs and applying further conditions on magnetic coordinates
-    grnd_temp = pd.DataFrame({'DT': sat_date, 'GDLAT': sat_glat, 'GLON': sat_glon, 'TEC': sat_tec, 'MLAT': sat_mlat, 'MLON': sat_mlon, 'MLT': sat_mlt})
-    grnd_tec1 = grnd_temp.sort_values(by=['DT', 'GDLAT'], ascending=[True, True]).reset_index()
-    grnd_tec1 = grnd_tec1[(grnd_tec1.MLON <= 5) & (grnd_tec1.MLON >= -5)].reset_index(drop=True)
-    grnd_tec1 = grnd_tec1[(grnd_tec1.MLAT <= 40) & (grnd_tec1.MLAT >= -40)].reset_index(drop=True)
-    grnd_tec2 = grnd_tec1.drop(['GDLAT', 'GLON','MLON'], axis = 1)
+    X, Y = np.meshgrid(result.MLT_b.unique(), result.MLAT_b.unique())
+    Z = result.TEC.values.reshape(X.shape)
+    contour_levels = list(range(0, 101, 10))
+
+    # Creating new dataframe to remove rows where TEC= NaN
+    result_n = pd.DataFrame({'mlt': result_ind.mlt, 'mlat_n': result_ind.mlat_n, 'tec_n': result_ind.tec_n})
+    result_n = result_n.dropna(subset='tec_n')
+    result_s = pd.DataFrame({'mlt': result_ind.mlt, 'mlat_s': result_ind.mlat_s, 'tec_s': result_ind.tec_s})
+    result_s = result_s.dropna(subset='tec_s')
 
     
-    # Writing the output into csv files for easy post processing
-    grnd_tec2.to_csv('/home/pxv220016/scratch/Qingyu_Cesar_EIA/outputs/' + month + '/' + str(year) + '_' + month + '_equinox.csv', index=False)
+    fig = plt.figure(figsize=(12,4))
+    specs = fig.add_gridspec(1, 2, width_ratios=[1,0.1])
+    ax = []
+    ax.append(fig.add_subplot(specs[0, 0]))
+    c = ax[0].contourf(X, Y, Z, levels=contour_levels, cmap = 'jet')
+    ax[0].scatter(result_n.mlt, result_n.mlat_n, c='b', s=30)
+    ax[0].scatter(result_s.mlt, result_s.mlat_s, c='b', s=30)
+    ax[0].set_title('Tracing EIA Peaks - ' + str(year) + ' ' + month + ' Equinox (Bins -> MLAT - 1 deg, MLT = 0.25 hour)')
+    ax[0].set_ylabel('MLAT')
+    ax[0].set_xlabel('MLT')
+    ax[0].grid(True)
+    
+    cbar_ax = fig.add_subplot(specs[0,1])
+    cbar = fig.colorbar(c, cax=cbar_ax, label='VTEC (TECUnits)', extend='both')
+    fig.savefig('/home/pxv220016/prasoon/data/sat_interp_repo/repo2/Qingyu_Cesar_EIA_IHA/outputs/eia_peaks/'  + month + '/eia_peaks_'  + month + '_' + str(year) + '.jpg')
+    fig.show()
+
+
