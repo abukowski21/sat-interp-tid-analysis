a53a516519529cd2793856ef0d7c4aa1cb158e3c
Prasoon Vishwakarma <prasoonv@Prasoons-iMac.local>
2024-11-19 21:58:27 -0600

    Producing the neutral winds plots for Gannon storm - Soumya project

---
+++ utility_programs/gitm_to_array.py
@@ -0,0 +1,34 @@
+import numpy as np
+import xarray as xr
+import gitm
+
+
+
+# Load the GITM binary file
+gitm_bin = gitm.Gitmbin("filename.bin")
+
+def gitm_to_xarray(gitm_bin):
+    # Extract attributes from the GITM binary file
+    attrs = gitm_bin.attrs
+    nLon = attrs['nLon']  # Number of longitude grid points
+    nLat = attrs['nLat']  # Number of latitude grid points
+    nAlt = attrs['nAlt']  # Number of altitude grid points
+    nVars = attrs['nVars']  # Number of variables
+
+    # Extract data and reshape it
+    data_vars = {}
+    for key in list(gitm_bin.keys())[1:]:  # Skip the first key if it's not a data variable
+        data = np.array(gitm_bin[key])  # Convert data to a NumPy array
+        data = data.reshape((nLon, nLat, nAlt))  # Reshape data to match the grid dimensions
+        data_vars[key] = (['lon', 'lat', 'alt'], data)  # Store data with corresponding coordinates
+
+    # Define coordinates for the xarray.Dataset
+    coords = {
+        'lon': gitm_bin['Longitude'][:, 0, 0],  # Longitude values
+        'lat': gitm_bin['Latitude'][0, :, 0],  # Latitude values
+        'alt': gitm_bin['Altitude'][0, 0, :],  # Altitude values
+    }
+
+    # Create an xarray.Dataset with the data variables and coordinates
+    ds = xr.Dataset(data_vars, coords=coords, attrs=attrs)
+    return ds
